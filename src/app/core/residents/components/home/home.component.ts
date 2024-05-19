3f0269ab3502085544c361de153707b4b24ddde2
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 15:44:38 +0400

    #974 - Frontend - Facility Type Independent Living (973)

--- src/app/core/residents/components/home/home.component.ts
+++ src/app/core/residents/components/home/home.component.ts
@@ -1,15 +1,16 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {simpleEmptyImage} from 'ng-zorro-antd';
-import {DomSanitizer} from '@angular/platform-browser';
-import {first} from 'rxjs/operators';
-import {GroupType} from '../../models/group-type.enum';
-import {AuthGuard} from '../../../guards/auth.guard';
-import {ResidentAdmissionService} from '../../services/resident-admission.service';
 import {Router} from '@angular/router';
+import {DomSanitizer} from '@angular/platform-browser';
+import {simpleEmptyImage} from 'ng-zorro-antd';
 import {Subscription} from 'rxjs';
-import {Facility} from '../../models/facility';
+import {first} from 'rxjs/operators';
+import {AuthGuard} from '../../../guards/auth.guard';
 import {FacilityService} from '../../services/facility.service';
-import {ResidentSelectorService} from '../../services/resident-selector.service';
+import {ApartmentService} from '../../services/apartment.service';
+import {RegionService} from '../../services/region.service';
+import {ResidentAdmissionService} from '../../services/resident-admission.service';
+import {GroupType} from '../../models/group-type.enum';
+import {GroupHelper} from '../../helper/group-helper';
 
 @Component({
   templateUrl: './home.component.html',
@@ -17,43 +18,43 @@ import {ResidentSelectorService} from '../../services/resident-selector.service'
   providers: []
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  GROUP_TYPE = GroupType;
   defaultSvg = this.sanitizer.bypassSecurityTrustResourceUrl(simpleEmptyImage);
+  GroupType = GroupType;
 
   data: any;
-  residents: any;
-  facilities: Facility[];
 
-  type: number;
-  type_id: number;
-  type_name: string;
+  options: { state?: string, type?: number, type_id?: number, sort_resident?: boolean, sort_room?: boolean } = {
+    sort_resident: null,
+    sort_room: null,
+  };
+
+  group_helper: GroupHelper;
 
   resident_id: number;
+  residents: any;
 
-  sort_resident: boolean;
-  sort_room: boolean;
+  type_name: string;
 
   protected $subscriptions: { [key: string]: Subscription; };
 
   constructor(
     private sanitizer: DomSanitizer,
     private facility$: FacilityService,
+    private apartment$: ApartmentService,
+    private region$: RegionService,
     private residentAdmission$: ResidentAdmissionService,
-    private residentSelector$: ResidentSelectorService,
     private router$: Router,
     private auth_$: AuthGuard
   ) {
+    this.group_helper = new GroupHelper();
+
     this.$subscriptions = {};
-
-    this.sort_resident = null;
-    this.sort_room = null;
-
-    this.resident_id = null;
-    this.type_name = null;
   }
 
   ngOnInit(): void {
     this.subscribe('list_facility');
+    this.subscribe('list_apartment');
+    this.subscribe('list_region');
   }
 
   ngOnDestroy(): void {
@@ -65,18 +66,41 @@ export class HomeComponent implements OnInit, OnDestroy {
       case 'list_facility':
         this.$subscriptions[key] = this.facility$.all().pipe(first()).subscribe(res => {
           if (res) {
-            res.sort((a, b) => a.id - b.id);
-            this.facilities = res;
+            this.group_helper.facilities = res;
+            this.group_helper.facilities.forEach((v, i) => {
+              this.group_helper.facilities[i]['type'] = GroupType.FACILITY;
+            });
 
-            if (this.facilities.length > 0) {
-              this.select_facility(this.facilities[0].id, this.facilities[0].name);
+            if (this.group_helper.facilities.length > 0) {
+              this.select_group(GroupType.FACILITY, this.group_helper.facilities[0].id, this.group_helper.facilities[0].name);
             }
           }
         });
         break;
-      case 'list_facility_residents':
+      case 'list_apartment':
+        this.$subscriptions[key] = this.apartment$.all().pipe(first()).subscribe(res => {
+          if (res) {
+            this.group_helper.apartments = res;
+            this.group_helper.apartments.forEach((v, i) => {
+              this.group_helper.apartments[i]['type'] = GroupType.APARTMENT;
+            });
+          }
+        });
+        break;
+      case 'list_region':
+        this.$subscriptions[key] = this.region$.all().pipe(first()).subscribe(res => {
+          if (res) {
+            this.group_helper.regions = res;
+            this.group_helper.regions.forEach((v, i) => {
+              this.group_helper.regions[i]['type'] = GroupType.REGION;
+            });
+          }
+        });
+        break;
+      case 'list_residents':
+        const state = (this.options.state !== undefined && this.options.state !== null) ? this.options.state : 'active';
         this.$subscriptions[key] = this.residentAdmission$
-          .list_by_state('active', params.type, params.type_id)
+          .list_by_state(state, params.type, params.type_id)
           .pipe(first()).subscribe(res => {
             if (res) {
               this.residents = res;
@@ -86,15 +110,15 @@ export class HomeComponent implements OnInit, OnDestroy {
       case 'list_active_first':
         const request_params: any = {type: params.type, type_id: params.type_id};
 
-        if (this.sort_resident === true) {
+        if (this.options.sort_resident === true) {
           request_params['resident'] = 'ASC';
-        } else if (this.sort_resident === false) {
+        } else if (this.options.sort_resident === false) {
           request_params['resident'] = 'DESC';
         }
 
-        if (this.sort_room === true) {
+        if (this.options.sort_room === true) {
           request_params['room'] = 'ASC';
-        } else if (this.sort_room === false) {
+        } else if (this.options.sort_room === false) {
           request_params['room'] = 'DESC';
         }
 
@@ -102,9 +126,17 @@ export class HomeComponent implements OnInit, OnDestroy {
           .list_active_first(request_params.type, request_params.type_id, request_params.resident, request_params.room)
           .pipe(first()).subscribe(res => {
             if (res) {
-              this.data = res;
-
-              this.subscribe('list_facility_residents', {type: GroupType.FACILITY, type_id: this.type_id});
+              switch (request_params.type) {
+                case GroupType.FACILITY:
+                  this.data = res.facility;
+                  break;
+                case GroupType.APARTMENT:
+                  this.data = res.apartment;
+                  break;
+                case GroupType.REGION:
+                  this.data = res.region;
+                  break;
+              }
             }
           });
         break;
@@ -123,27 +155,23 @@ export class HomeComponent implements OnInit, OnDestroy {
     return this.auth_$.checkPermission([permission]);
   }
 
-  select_facility(id: number, name: string) {
-    this.type_id = id;
-    this.type_name = name;
-    this.resident_id = null;
-
-    this.subscribe('list_active_first', {type: GroupType.FACILITY, type_id: this.type_id});
-  }
-
   sort(sort_column: string) {
     switch (sort_column) {
       case 'resident':
-        this.sort_resident = this.tristate(this.sort_resident);
-        this.sort_room = null;
+        this.options.sort_resident = this.tristate(this.options.sort_resident);
+        this.options.sort_room = null;
+
+        this.options = Object.assign({}, this.options);
         break;
       case 'room':
-        this.sort_resident = null;
-        this.sort_room = this.tristate(this.sort_room);
+        this.options.sort_resident = null;
+        this.options.sort_room = this.tristate(this.options.sort_room);
+
+        this.options = Object.assign({}, this.options);
         break;
     }
 
-    this.subscribe('list_active_first', {type: GroupType.FACILITY, type_id: this.type_id});
+    this.subscribe('list_active_first', {type: this.options.type, type_id: this.options.type_id});
   }
 
   tristate(state: boolean) {
@@ -156,6 +184,19 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
   }
 
+  select_group(type: number, id: number, name: string) {
+    this.type_name = name;
+    this.resident_id = null;
+
+    this.subscribe('list_residents', {type: type, type_id: id});
+
+    this.options.type = type;
+    this.options.type_id = id;
+    this.options = Object.assign({}, this.options);
+
+    this.subscribe('list_active_first', {type: this.options.type, type_id: this.options.type_id});
+  }
+
   resident_changed() {
     if (this.resident_id !== null) {
       this.router$.navigate(
@@ -163,4 +204,9 @@ export class HomeComponent implements OnInit, OnDestroy {
       );
     }
   }
+
+  show_room_sort(): boolean {
+    return this.options.type !== GroupType.REGION;
+  }
+
 }
