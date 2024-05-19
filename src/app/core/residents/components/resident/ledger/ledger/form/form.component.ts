1fd89ba1402a44496e1f21725550fb02dea2f9b3
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 15:44:42 +0400

    #SC-1098 - Discount Item Change: Allow user to add differ <= amount.

--- src/app/core/residents/components/resident/ledger/ledger/form/form.component.ts
+++ src/app/core/residents/components/resident/ledger/ledger/form/form.component.ts
@@ -24,156 +24,165 @@ import {LatePayment} from '../../../../../models/late-payment';
 import {LatePaymentService} from '../../../../../services/late-payment.service';
 
 @Component({
-    templateUrl: 'form.component.html'
+  templateUrl: 'form.component.html'
 })
 export class FormComponent extends AbstractForm implements OnInit {
-    source_selector: number = null;
+  source_selector: number = null;
 
-    payment_sources: PaymentSource[];
+  payment_sources: PaymentSource[];
 
-    sources: { id: number, amount: number }[] = [];
+  sources: { id: number, amount: number }[] = [];
 
-    credit_items: CreditItem[];
-    discount_items: DiscountItem[];
-    payment_types: RpPaymentType[];
-    responsible_persons: ResidentResponsiblePerson[];
+  credit_items: CreditItem[];
+  discount_items: DiscountItem[];
+  payment_types: RpPaymentType[];
+  responsible_persons: ResidentResponsiblePerson[];
 
-    late_payments: LatePayment[];
+  late_payments: LatePayment[];
 
-    private query_params: Params;
+  private query_params: Params;
 
-    public isThirdParty: boolean;
-    public currentDiscountItemId: number;
-    public currentCreditItemId: number;
+  public isThirdParty: boolean;
+  public currentDiscountItemId: number;
+  public currentCreditItemId: number;
 
-    formatterDollar = (value: number) => (new CurrencyPipe('en-US')).transform(value, 'USD', 'symbol-narrow', '1.2-2');
+  formatterDollar = (value: number) => (new CurrencyPipe('en-US')).transform(value, 'USD', 'symbol-narrow', '1.2-2');
 
-    button_loading: Array<boolean>;
+  button_loading: Array<boolean>;
 
-    @ViewChild('addCreditItem', {static: false}) btn_add_credit_item;
-    @ViewChild('addDiscountItem', {static: false}) btn_add_discount_item;
-    @ViewChild('addPrivatePayPaymentReceivedItem', {static: false}) btn_add_private_pay_payment_received_item;
-    @ViewChild('addNotPrivatePayPaymentReceivedItem', {static: false}) btn_add_not_private_pay_payment_received_item;
+  @ViewChild('addCreditItem', {static: false}) btn_add_credit_item;
+  @ViewChild('addDiscountItem', {static: false}) btn_add_discount_item;
+  @ViewChild('addPrivatePayPaymentReceivedItem', {static: false}) btn_add_private_pay_payment_received_item;
+  @ViewChild('addNotPrivatePayPaymentReceivedItem', {static: false}) btn_add_not_private_pay_payment_received_item;
 
-    constructor(
-        protected modal$: ModalFormService,
-        private formBuilder: FormBuilder,
-        private residentSelector$: ResidentSelectorService,
-        private route$: ActivatedRoute,
-        private ledger$: ResidentLedgerService,
-        private payment_source$: PaymentSourceService,
-        private credit_item$: CreditItemService,
-        private discount_item$: DiscountItemService,
-        private payment_type$: RpPaymentTypeService,
-        private responsible_person$: ResidentResponsiblePersonService,
-        private late_payment$: LatePaymentService,
-    ) {
-        super(modal$);
+  constructor(
+    protected modal$: ModalFormService,
+    private formBuilder: FormBuilder,
+    private residentSelector$: ResidentSelectorService,
+    private route$: ActivatedRoute,
+    private ledger$: ResidentLedgerService,
+    private payment_source$: PaymentSourceService,
+    private credit_item$: CreditItemService,
+    private discount_item$: DiscountItemService,
+    private payment_type$: RpPaymentTypeService,
+    private responsible_person$: ResidentResponsiblePersonService,
+    private late_payment$: LatePaymentService,
+  ) {
+    super(modal$);
 
-        this.button_loading = new Array<boolean>(100);
-    }
+    this.button_loading = new Array<boolean>(100);
+  }
 
-    ngOnInit(): void {
-        this.form = this.formBuilder.group({
-            id: [''],
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      id: [''],
 
-            source: this.formBuilder.array([]),
+      source: this.formBuilder.array([]),
 
-            resident_credit_items: this.formBuilder.array([]),
-            resident_discount_items: this.formBuilder.array([]),
-            resident_private_pay_payment_received_items: this.formBuilder.array([]),
-            resident_not_private_pay_payment_received_items: this.formBuilder.array([]),
+      resident_credit_items: this.formBuilder.array([]),
+      resident_discount_items: this.formBuilder.array([]),
+      resident_private_pay_payment_received_items: this.formBuilder.array([]),
+      resident_not_private_pay_payment_received_items: this.formBuilder.array([]),
 
-            late_payment_id: [null, Validators.compose([])],
+      late_payment_id: [null, Validators.compose([])],
 
-            resident_id: [null, Validators.required]
+      resident_id: [null, Validators.required]
+    });
+
+    this.subscribe('rs_resident');
+    this.subscribe('query_map');
+    this.subscribe('list_payment_source');
+    this.subscribe('list_payment_type');
+    this.subscribe('list_resident_responsible_person');
+    this.subscribe('list_late_payment');
+  }
+
+  protected subscribe(key: string, params?: any): void {
+    switch (key) {
+      case 'list_payment_source':
+        this.$subscriptions[key] = this.payment_source$.all().pipe(first()).subscribe(res => {
+          if (res) {
+            this.payment_sources = res;
+          }
         });
+        break;
+      case 'rs_resident':
+        this.$subscriptions[key] = this.residentSelector$.resident.subscribe(next => {
+          if (next) {
+            this.form.get('resident_id').setValue(next);
+          }
+        });
+        break;
+      case 'query_map':
+        this.$subscriptions[key] = this.route$.queryParams.subscribe(query_params => {
+          this.query_params = query_params;
+        });
+        break;
+      case 'list_credit_item':
+        this.$subscriptions[key] = this.credit_item$.all([{
+          key: 'ledger_id',
+          value: params.ledger_id
+        }]).pipe(first()).subscribe(res => {
+          if (res) {
+            this.credit_items = res;
+          }
+        });
+        break;
+      case 'list_discount_item':
+        this.$subscriptions[key] = this.discount_item$.all([{
+          key: 'ledger_id',
+          value: params.ledger_id
+        }]).pipe(first()).subscribe(res => {
+          if (res) {
+            this.discount_items = res;
+          }
+        });
+        break;
+      case 'list_payment_type':
+        this.$subscriptions[key] = this.payment_type$.all().pipe(first()).subscribe(res => {
+          if (res) {
+            this.payment_types = res;
 
-        this.subscribe('rs_resident');
-        this.subscribe('query_map');
-        this.subscribe('list_payment_source');
-        this.subscribe('list_payment_type');
-        this.subscribe('list_resident_responsible_person');
-        this.subscribe('list_late_payment');
+            if (params) {
+              this.form.get('payment_type_id').setValue(params.payment_type_id);
+            }
+          }
+        });
+        break;
+      case 'list_resident_responsible_person':
+        this.$subscriptions[key] = this.responsible_person$.all([{
+          key: 'resident_id',
+          value: this.query_params['resident_id']
+        }, {key: 'financially', value: true}]).pipe(first()).subscribe(res => {
+          if (res) {
+            this.responsible_persons = res;
+
+            if (params) {
+              this.form.get('responsible_person_id').setValue(params.responsible_person_id);
+            }
+          }
+        });
+        break;
+      case 'list_late_payment':
+        this.$subscriptions[key] = this.late_payment$.all().pipe(first()).subscribe(res => {
+          if (res) {
+            this.late_payments = res;
+
+            if (params) {
+              this.form.get('late_payment_id').setValue(params.late_payment_id);
+            }
+          }
+        });
+        break;
+      default:
+        break;
     }
+  }
 
-    protected subscribe(key: string, params?: any): void {
-        switch (key) {
-            case 'list_payment_source':
-              this.$subscriptions[key] = this.payment_source$.all().pipe(first()).subscribe(res => {
-                if (res) {
-                  this.payment_sources = res;
-                }
-              });
-              break;
-            case 'rs_resident':
-              this.$subscriptions[key] = this.residentSelector$.resident.subscribe(next => {
-                if (next) {
-                  this.form.get('resident_id').setValue(next);
-                }
-              });
-              break;
-            case 'query_map':
-              this.$subscriptions[key] = this.route$.queryParams.subscribe(query_params => {
-                this.query_params = query_params;
-              });
-              break;
-            case 'list_credit_item':
-              this.$subscriptions[key] = this.credit_item$.all([{key: 'ledger_id', value: params.ledger_id}]).pipe(first()).subscribe(res => {
-                if (res) {
-                  this.credit_items = res;
-                }
-              });
-              break;
-            case 'list_discount_item':
-              this.$subscriptions[key] = this.discount_item$.all([{key: 'ledger_id', value: params.ledger_id}]).pipe(first()).subscribe(res => {
-                if (res) {
-                  this.discount_items = res;
-                }
-              });
-             break;
-            case 'list_payment_type':
-              this.$subscriptions[key] = this.payment_type$.all().pipe(first()).subscribe(res => {
-                if (res) {
-                  this.payment_types = res;
-
-                  if (params) {
-                    this.form.get('payment_type_id').setValue(params.payment_type_id);
-                  }
-                }
-              });
-              break;
-            case 'list_resident_responsible_person':
-              this.$subscriptions[key] = this.responsible_person$.all([{key: 'resident_id', value: this.query_params['resident_id']}, {key: 'financially', value: true}]).pipe(first()).subscribe(res => {
-                if (res) {
-                  this.responsible_persons = res;
-
-                  if (params) {
-                    this.form.get('responsible_person_id').setValue(params.responsible_person_id);
-                  }
-                }
-              });
-              break;
-            case 'list_late_payment':
-              this.$subscriptions[key] = this.late_payment$.all().pipe(first()).subscribe(res => {
-                if (res) {
-                  this.late_payments = res;
-
-                  if (params) {
-                    this.form.get('late_payment_id').setValue(params.late_payment_id);
-                  }
-                }
-              });
-              break;
-            default:
-                break;
-        }
-    }
-
-    public after_set_form_data(): void {
-      this.subscribe('list_credit_item', {ledger_id: this.form.get('id').value});
-      this.subscribe('list_discount_item', {ledger_id: this.form.get('id').value});
-    }
+  public after_set_form_data(): void {
+    this.subscribe('list_credit_item', {ledger_id: this.form.get('id').value});
+    this.subscribe('list_discount_item', {ledger_id: this.form.get('id').value});
+  }
 
   public get_form_array_skeleton(key: string): FormGroup {
     switch (key) {
