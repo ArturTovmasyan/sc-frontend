e2ef0801b9315e69a5972bde3e8a33fa7b742a48
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 15:44:20 +0400

    Fixed null value pipe errors, fixed pdf url.

--- src/app/shared/services/grid.service.ts
+++ src/app/shared/services/grid.service.ts
@@ -35,7 +35,7 @@ export class GridService<T extends IdInterface> {
 
   public pdf(callback: any) {
     return this.http
-      .get(this.SEVICE_URL_BASE + '/list', {
+      .get(this.SEVICE_URL_BASE + '', {
         responseType: 'blob',
         observe: 'response',
         params: new HttpParams().append('pdf', '1')
