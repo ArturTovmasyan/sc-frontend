6bbc545889f63f281d25e62c38cc5707532598e3
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 15:44:21 +0400

    Changed medication title length limit.

--- src/app/core/residents/components/medication/form/form.component.ts
+++ src/app/core/residents/components/medication/form/form.component.ts
@@ -18,7 +18,7 @@ export class FormComponent extends AbstractForm implements OnInit {
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       id: [''],
-      title: ['', Validators.compose([Validators.required, Validators.max(200)])],
+      title: ['', Validators.compose([Validators.required, Validators.max(255)])],
 
       space_id: [null, Validators.required],
     });
