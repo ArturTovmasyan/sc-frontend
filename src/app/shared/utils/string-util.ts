4b13f830501a668b7b6fadf82293ce9a06683a76
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 15:44:37 +0400

    #962 - Sloppy column layout on Facility Room Base Room - this problem exists in many, many screens and is not a good experience for our users

--- src/app/shared/utils/string-util.ts
+++ src/app/shared/utils/string-util.ts
@@ -2,4 +2,8 @@ export class StringUtil {
   public static truncate(value: string, length: number): string {
     return value.length > length ? (value.slice(0, length - 3) + '...') : value;
   }
+
+  public static extension(value: string): string {
+    return value.slice((Math.max(0, value.lastIndexOf(".")) || Infinity) + 1);
+  }
 }
